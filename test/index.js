f389486541bdc329aeefb28a84904f1f8282eefa
vanarchist <v4narchist@gmail.com>
2019-06-28 12:27:42 -0400

    Added test placeholder

---
+++ test/index.js
@@ -0,0 +1,28 @@
+const path = require('path')
+const tape = require('tape')
+
+const { Diorama, tapeExecutor, backwardCompatibilityMiddleware } = require('@holochain/diorama')
+
+process.on('unhandledRejection', error => {
+  // Will print "unhandledRejection err is not defined"
+  console.error('got unhandledRejection:', error);
+});
+
+const dnaPath = path.join(__dirname, "../dist/holochain-mutual-credit-clearing.dna.json")
+const dna = Diorama.dna(dnaPath, 'holochain-mutual-credit-clearing')
+
+const diorama = new Diorama({
+  instances: {
+    alice: dna,
+    bob: dna,
+  },
+  bridges: [],
+  debugLog: false,
+  executor: tapeExecutor(require('tape')),
+  middleware: backwardCompatibilityMiddleware,
+})
+
+require('./scenarios')(diorama.registerScenario)
+
+diorama.run()
+
---
+++ test/scenarios.js
@@ -0,0 +1,5 @@
+module.exports = scenario => {
+  scenario("Scenario 1", async (s, t, { alice, bob }) => {
+          console.log("Scenario 1")
+  })
+}
