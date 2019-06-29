6cedecd0e66125c8e78f7fdbd1ef222f2635b5ab
vanarchist <v4narchist@gmail.com>
2019-06-29 12:55:20 -0400

    Trying to get tests to work

--- test/scenarios.js
+++ test/scenarios.js
@@ -1,5 +1,7 @@
 module.exports = scenario => {
-  scenario("Scenario 1", async (s, t, { alice, bob }) => {
-          console.log("Scenario 1")
+  scenario("User registration", async (s, t, { alice, bob }) => {
+    // user list empty to start
+    const users = await alice.call('mutual_credit_clearing', 'get_users');
+    t.equal(users.length, 0, "User list empty to start")
   })
 }
